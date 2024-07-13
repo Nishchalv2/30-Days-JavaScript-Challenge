ed06ea2a9636b4aca9f6acf3b20076d6e187400a
Nishchalv2 <nishchalck3825@gmail.com>
2024-07-13 14:50:59 +0530

    Day_01 Challenge

---
+++ Day_01.js
@@ -0,0 +1,41 @@
+// Activity 1
+var num = 1;
+console.log(num);
+
+let str = "Hello World !!";
+console.log(str);
+
+// Activity 2
+const isValidAge = false;
+console.log(isValidAge);
+
+// Activity 3
+let number = 1;
+let string = "Helooo";
+let isCorrect = false;
+let array = [1, 2, 3, 4];
+let obj = {
+  name: "Nishchal",
+};
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof isCorrect);
+console.log(typeof array);
+console.log(typeof obj);
+
+// Activity 4
+
+let num1 = 2;
+console.log("before Reassigning : ", num1);
+num1 = 3;
+console.log("after Reassigning : ", num1);
+
+// Activity 5
+
+const pi = 3.14;
+console.log("before Reassigning : ", pi);
+
+// Uncomment to see the error
+// pi = 2.3;
+// console.log("before Reassigning : ", p1);
